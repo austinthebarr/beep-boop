3b8f85047c8db99ad6f6bd2a7d34d6af2c2654cd
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-08 16:22:54 -0700

    got program to register digits after 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
 
 
-
+// Buisness Logic
 var inputNumber = function(userInput){
 // this will take counted data from user
   var countArray = [];
-  // this will make countArray
+  // this loop will make countArray
+  debugger;
     for (var index = 0; index <= userInput; index+= 1) {
-
       if (index === 0){
-        countArray.splice(index,1,"beep!")
+        countArray.push("beep!")
       }
       else if ((index % 3) === 0) {
-        countArray.splice(index,1,"I'm sorry, Dave. I'm afraid I can't do that")
+        countArray.push("I'm sorry, Dave. I'm afraid I can't do that")
+      }
+      else if (index === 1) {
+        countArray.push("boop!")
+      }
+      else if ((index % 10) >= 1){
+        countArray.push("beep!")
       }
       else if (index !== 0 && index !== 1 && index % 3 !== 0){
         countArray.push(index);
       }
-      else if (index === 1) {
-        countArray.splice(index,1,"boop!")
-      }
       $("span#user-output").text(countArray)
-
   }
-
 }
 
-
+// userInput
 $(function(){
   $("form#input-form").submit(function(event) {
     event.preventDefault();
