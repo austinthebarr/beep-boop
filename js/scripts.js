5cb67ed7d9b94abe0ae650a39ca454822453ea41
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-08 09:30:40 -0700

    add first logic; to create a list of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 $(function(){
+
+var inputNumber = function(userInput){
+
+  for (var index = 0; index <= userInput; index++) {
+    return index;
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
   $("form#input-form").submit(function(event) {
     event.preventDefault();
-    var userInput = $('input#user-input').val();
-    console.log(userInput)
+    var userInput = parseInt($('input#user-input').val());
+  console.log(inputNumber(userInput))
   });
 
 });
