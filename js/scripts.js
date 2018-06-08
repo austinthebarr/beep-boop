334ce240833fcb01d5c1cf72d6d146c73dd0d654
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-08 16:03:32 -0700

    program works, almost// does not recoginzes 11, 12, etc

--- js/scripts.js
+++ js/scripts.js
@@ -3,92 +3,33 @@
 
 var inputNumber = function(userInput){
 // this will take counted data from user
-// debugger;
   var countArray = [];
   // this will make countArray
     for (var index = 0; index <= userInput; index+= 1) {
-      // debugger;
-      var userCount = index;
-      countArray.push(index);
-      if(index === 1){
-        countArray.push("boop")
+
+      if (index === 0){
+        countArray.splice(index,1,"beep!")
       }
-      else if(index === 0){
-        countArray.push("beep")
+      else if ((index % 3) === 0) {
+        countArray.splice(index,1,"I'm sorry, Dave. I'm afraid I can't do that")
       }
-      if(index % 3 === 0){
-        countArray.push("Im sorry Dave. Im afraid I cant do that")
+      else if (index !== 0 && index !== 1 && index % 3 !== 0){
+        countArray.push(index);
       }
+      else if (index === 1) {
+        countArray.splice(index,1,"boop!")
+      }
+      $("span#user-output").text(countArray)
 
   }
- // This will create a new array from list created by user
-  var afterCountArray = countArray.slice()
-  // var test = afterCountArray.split('')
-//this will combine all chacters and split each one up
-var arrayString = afterCountArray.toString()
-var newArra2 = [];
-var newArra = newArra2.push(arrayString)
-var parseIntTest = parseInt(arrayString)
-var SplitArra2 = newArra2.join('')
-//
-// parseIntTest.forEach(){};
-
-
-//
-// var parseString = parseInt(arrayString)
-// var stringSplit = parseString.split("")
-
-// var index = arrayString.indexOf("1")
-// var index2 = arrayString.indexOf("2")
-//  if (index !== -1){
-//    arrayString[index] = "beep";
-//    }
-//  if (index2 !== -1){
-//    arrayString[index2] = "boop"
-//  }
-
-
-
-
-
-// this array will change each number to create response
- // afterCountArray.forEach(function(afterCountArra){
- //    if(afterCountArra === 0){
- //      afterCountArray.splice()
- //    }
- // console.log(parseArray)
- // console.log(stringSplit)
- console.log(SplitArra2)
- console.log(afterCountArray)
- console.log(parseIntTest)
-console.log(countArray)
-console.log(arrayString)
-console.log(newArra2)
 
 }
 
 
-
-
-  //this map will sort through countArray
-
-
-
-
-
-
-
 $(function(){
-
-
-
-
   $("form#input-form").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($('input#user-input').val());
   console.log(inputNumber(userInput))
-
-
   });
-
 });
