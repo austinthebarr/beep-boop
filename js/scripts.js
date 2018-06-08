1095f334a4d2c42e14997ab5a59743131d277968
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-08 11:52:33 -0700

    caught 1 and 0 turned them into beep and boop but was done inncorectly

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,48 @@
 var inputNumber = function(userInput){
 // this will take counted data from user
 
-var countArray = [];
-// this will make countArray
-  for (var index = 0; index <= userInput; index+= 1) {
-    var userCount = index;
-    countArray.push(index);
-}
-var afterCount = countArray.slice()
-var string = countArray.toString();
-console.log(afterCount)
+  var countArray = [];
+  // this will make countArray
+    for (var index = 0; index <= userInput; index+= 1) {
+      // debugger;
+      var userCount = index;
+      countArray.push(index);
+  }
+ // This will create a new array from list created by user
+  var afterCountArray = countArray.slice()
+
+ var index = afterCountArray.indexOf(0)
+ var index2 = afterCountArray.indexOf(1)
+  if (index !== -1){
+    afterCountArray[index] = "beep";
+    }
+  if (index2 !== -1){
+    afterCountArray[index2] = "boop"
+  }
+  // var outputForUser = function(afterCountArray){
+
+
+// for looop
+    // for (var index= 0; index <= afterCountArray.length; index++){
+    //   if (afterCountArra.indexOf("0") === 0 ){
+    //     afterCountArray.splice( index ,s"boo")
+    //   }
+    // }
+
+// this array will change each number to create response
+ // afterCountArray.forEach(function(afterCountArra){
+ //    if(afterCountArra === 0){
+ //      afterCountArray.splice()
+ //    }
+console.log(afterCountArray)
+  // }
+
 }
 
-//this map will sort through countArray
+
+
+
+  //this map will sort through countArray
 
 
 
