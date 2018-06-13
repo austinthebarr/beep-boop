35b3c11a4f43024e8dd4dfa17c780071f220cd4a
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-06-12 19:34:32 -0700

    fixed bug

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,26 @@
 
 // Buisness Logic
 var inputNumber = function(userInput){
+
+
 // this will take counted data from user
   var countArray = [];
   // this loop will make countArray
   debugger;
     for (var index = 0; index <= userInput; index+= 1) {
-      if (index === 0){
+      if(index === 0){
         countArray.push("beep!")
       }
-      else if ((index % 3) === 0) {
-        countArray.push("I'm sorry, Dave. I'm afraid I can't do that")
+     else if ((index % 3) === 0) {
+      countArray.push("I'm sorry," + userName + ". I'm afraid I can't do that")
       }
-      else if (index === 1) {
+      else if (index.toString().includes(1) === true) {
         countArray.push("boop!")
       }
-      else if ((index % 10) >= 1){
+      else if (index.toString().includes(0) === true){
         countArray.push("beep!")
       }
-      else if (index !== 0 && index !== 1 && index % 3 !== 0){
+      else {
         countArray.push(index);
       }
       $("span#user-output").text(countArray)
@@ -30,6 +32,7 @@ var inputNumber = function(userInput){
 $(function(){
   $("form#input-form").submit(function(event) {
     event.preventDefault();
+        userName = $('input#user-name').val();
     var userInput = parseInt($('input#user-input').val());
   console.log(inputNumber(userInput))
   });
